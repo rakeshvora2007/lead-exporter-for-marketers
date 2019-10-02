442c3370d36512d8e08888763980ecfed9753fac
abir444 <mr.abir444@gmail.com>
2019-10-02 23:52:39 +0600

    your commit message

--- main.js
+++ main.js
@@ -8,14 +8,15 @@
 // })
 
 document.getElementById("filesub").addEventListener('change', function(){
-var fr = new FileReader(e);
+var fr = new FileReader();
 fr.readAsArrayBuffer(e.target.Files[0]);
-fr.onload = function(e){
+fr.onload = function(){
     var data = new Uint8Array(reader.result);
     var wb = XLSX.read(data,{type:'array'});
 
     var htmlstr = XLSX.write(wb,{sheet:"sheet no1",type:'binary',booktype:'html'});
-    document.getElementById("fileholder").[0].innerHTML +=htmlstr;
+    document.getElementById("fileholder").textContent = this.result;
+    fr.readAsBinaryString(this.files[0]);
 
 }
 })
