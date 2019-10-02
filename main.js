bc6e6353f8974a5814631aefca2f996be3908360
abir444 <mr.abir444@gmail.com>
2019-10-02 14:46:08 +0600

    your commit message

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-document.getElementById("filesub").addEventListener('change', function(){
-    var fr = new FileReader();
-    fr.onload = function(){
-        document.getElementById("fileholder").textContent = this.result;
+// document.getElementById("filesub").addEventListener('change', function(){
+//     var fr = new FileReader();
+//     fr.onload = function(e){
+//         document.getElementById("fileholder").textContent = this.result;
 
-    }
-    fr.readAsText(this.files[0]);
+//     }
+//     fr.readAsBinaryString(this.files[0]);
+// })
+
+document.getElementById("filesub").addEventListener('change', function(){
+var fr = new FileReader(e);
+fr.readAsArrayBuffer(e.target.Files[0]);
+fr.onload = function(e){
+    var data = new Uint8Array(reader.result);
+    var wb = XLSX.read(data,{type:'array'});
+
+    var htmlstr = XLSX.write(wb,{sheet:"sheet no1",type:'binary',booktype:'html'});
+    document.getElementById("fileholder").[0].innerHTML +=htmlstr;
+
+}
 })
